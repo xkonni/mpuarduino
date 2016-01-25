440616c4597c12fd19da3f4b4b5f520ef1e76442
Konstantin Koslowski <konstantin.koslowski@gmail.com>
2016-01-25 21:57:53 +0100

    mpuarduino: fix all the things
    * add some registers that are used
    * add a template vector struct for x,y,z
    * add function to write a single bit inside a register
    * add filter to smoothen the raw sensor readings
    * add getHeading and getTiltHeading functions
    
    major cleanup
    * add comments to all functions
    * remove unnecessary code

--- mpuarduino.ino
+++ mpuarduino.ino
@@ -1,41 +1,65 @@
 #include <Wire.h>
-#include <TimerOne.h>
+#include <math.h>
 
 #define    MPU9250_ADDRESS            0x68
 #define    MAG_ADDRESS                0x0C
+// REGISTERS
+#define    REG_MAG_CNTL1              0x0A
+#define    REG_INT_PIN_CFG            0x37
+// MASKS, VALUES
+#define    MASK_BYPASS_ENABLE         0x02
 
-#define    GYRO_FULL_SCALE_250_DPS    0x00  
+#define    GYRO_FULL_SCALE_250_DPS    0x00
 #define    GYRO_FULL_SCALE_500_DPS    0x08
 #define    GYRO_FULL_SCALE_1000_DPS   0x10
 #define    GYRO_FULL_SCALE_2000_DPS   0x18
 
-#define    ACC_FULL_SCALE_2_G        0x00  
+#define    ACC_FULL_SCALE_2_G        0x00
 #define    ACC_FULL_SCALE_4_G        0x08
 #define    ACC_FULL_SCALE_8_G        0x10
 #define    ACC_FULL_SCALE_16_G       0x18
 
+/*
+ * global variables
+ */
+// scale used for the accelerometer: 2 to 8
+int ACC_SCALE;
+// Initial time
+long int ti;
+// Output
+char printBuf[256];
 
 
-// This function read Nbytes bytes from I2C device at address Address. 
-// Put read bytes starting at register Register in the Data array. 
-void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data)
-{
+/*
+ * template vector
+ */
+template <typename T> struct vector {
+  T x;
+  T y;
+  T z;
+};
+
+/*
+ * read Nbytes from the register "Register" on I2C device at address "Address"
+ * update the *Data pointer with the read values
+ */
+void I2Cread(uint8_t Address, uint8_t Register, uint8_t Nbytes, uint8_t* Data) {
   // Set register address
   Wire.beginTransmission(Address);
   Wire.write(Register);
   Wire.endTransmission();
-  
+
   // Read Nbytes
-  Wire.requestFrom(Address, Nbytes); 
+  Wire.requestFrom(Address, Nbytes);
   uint8_t index=0;
   while (Wire.available())
     Data[index++]=Wire.read();
 }
 
-
-// Write a byte (Data) in device (Address) at register (Register)
-void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
-{
+/*
+ * write a byte "Data" in device on the I2C device at "Address" at "Register"
+ */
+void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data) {
   // Set register address
   Wire.beginTransmission(Address);
   Wire.write(Register);
@@ -43,162 +67,220 @@ void I2CwriteByte(uint8_t Address, uint8_t Register, uint8_t Data)
   Wire.endTransmission();
 }
 
+/*
+ * write a single bit
+ */
+void I2CwriteBit(uint8_t Address, uint8_t Register, uint8_t mask, boolean enabled) {
+  // get current register value
+  uint8_t data;
+  I2Cread(Address, Register, 1, &data);
+  // set bit
+  if (enabled) data = data | mask;
+  else data = data & ~mask;
+  // write
+  I2CwriteByte(Address, Register, data);
+}
 
+/*
+ * get accelerometer and gyroscope sensor values
+ */
+void getAccelGyro(vector<int16_t> *a, vector<int16_t> *g) {
+  // Read accelerometer and gyroscope
+  uint8_t Buf[14];
+  I2Cread(MPU9250_ADDRESS,0x3B,14,Buf);
 
-// Initial time
-long int ti;
-volatile bool intFlag=false;
+  // Create 16 bits values from 8 bits data
+  // .. Accelerometer
+  a->x=-(Buf[0]<<8 | Buf[1]);
+  a->y=-(Buf[2]<<8 | Buf[3]);
+  a->z=Buf[4]<<8 | Buf[5];
+  // .. Gyroscope
+  g->x=-(Buf[8]<<8 | Buf[9]);
+  g->y=-(Buf[10]<<8 | Buf[11]);
+  g->z=Buf[12]<<8 | Buf[13];
+}
 
-// Initializations
-void setup()
-{
-  // Arduino initializations
-  Wire.begin();
-  Serial.begin(115200);
-  Serial.println("testing...");
-   
+/*
+ * get magnetometer values
+ */
+void getMag(vector<int16_t> *m) {
+  // enable bypass
+  I2CwriteBit(MAG_ADDRESS, REG_INT_PIN_CFG, MASK_BYPASS_ENABLE, true);
+  // enable single measurement
+  I2CwriteByte(MAG_ADDRESS, REG_MAG_CNTL1, 0x01);
+
+  // wait for measurement to complete
+  uint8_t ST1;
+  do {
+    I2Cread(MAG_ADDRESS,0x02,1,&ST1);
+  } while (!(ST1&0x01));
+
+  // read magnetometer data
+  uint8_t Mag[7];
+  I2Cread(MAG_ADDRESS,0x03,7,Mag);
+
+  // create 16 bits values from 8 bits data
+  m->x=-(Mag[3]<<8 | Mag[2]);
+  m->y=-(Mag[1]<<8 | Mag[0]);
+  m->z=-(Mag[5]<<8 | Mag[4]);
+}
+
+/*
+ * get the current, tilt-compensated, heading
+ */
+double getTiltHeading(vector<int16_t> *a, vector<int16_t> *m) {
+  double heading;
+
+  // convert to [-1; 1]
+  vector<double> *realA = new vector<double>();
+  realA->x = (double) a->x / (1 << 15) * ACC_SCALE;
+  realA->y = (double) a->y / (1 << 15) * ACC_SCALE;
+  realA->z = (double) a->z / (1 << 15) * ACC_SCALE;
+
+  // DEBUG warnings
+  // if ((realA->x < -1) || (realA->x > 1)) {
+  //   Serial.println(realA->x, 5);
+  //   Serial.print("realA->x fail ");
+  //   // realA->x = realA->x/2;
+  // }
+  // if ((realA->y < -1) || (realA->y > 1)) {
+  //   Serial.println(realA->y, 5);
+  //   Serial.print("realA->y fail ");
+  //   // realA->y = realA->y/2;
+  // }
+  // if ((realA->z < -1) || (realA->z > 1)) {
+  //   Serial.println(realA->z, 5);
+  //   Serial.print("realA->z fail ");
+  //   realA->z = realA->z/2;
+  // }
+
+  float pitch = asin(-realA->x);
+  float roll = asin(realA->y/cos(pitch));
+  float xh = m->x * cos(pitch) + m->z * sin(pitch);
+  float yh = m->x * sin(roll) * sin(pitch) + m->y * cos(roll) - m->z * sin(roll) * cos(pitch);
+  // unused
+  // float zh = -m->x * cos(roll) * sin(pitch) + m->y * sin(roll) + m->z * cos(roll) * cos(pitch);
+
+  heading = round(180 * atan2(yh, xh)/PI);
+  if (heading < 0) heading += 360;
+
+  return (int(heading));
+}
+
+/*
+ * get the current heading, accurate when the sensor is lying flat on a surface
+ */
+int16_t getHeading(vector<int16_t> *m) {
+  int16_t heading;
+
+  double x = m->x;
+  double y = m->y;
+  heading = 180 * atan2(y, x) / PI;
+  if (heading < 0) heading += 360;
+
+  return (heading);
+}
+
+/*
+ * filter the current sample to smoothe out spikes in the readings
+ */
+void filter(vector<int16_t> *sample, vector<int16_t> *filtered, double coeff) {
+  // apply filter
+  sample->x = (sample->x * (1 - coeff)) + (filtered->x * coeff);
+  sample->y = (sample->y * (1 - coeff)) + (filtered->y * coeff);
+  sample->z = (sample->z * (1 - coeff)) + (filtered->z * coeff);
+  // also overwrite last filtered values
+  filtered->x = sample->x;
+  filtered->y = sample->y;
+  filtered->z = sample->z;
+}
+
+/*
+ * initialize the MPU9250
+ */
+void MPU9250_init() {
   // Set accelerometers low pass filter at 5Hz
   I2CwriteByte(MPU9250_ADDRESS,29,0x06);
   // Set gyroscope low pass filter at 5Hz
   I2CwriteByte(MPU9250_ADDRESS,26,0x06);
- 
-  
   // Configure gyroscope range
   I2CwriteByte(MPU9250_ADDRESS,27,GYRO_FULL_SCALE_1000_DPS);
   // Configure accelerometers range
   I2CwriteByte(MPU9250_ADDRESS,28,ACC_FULL_SCALE_4_G);
+  // DEBUG
+  ACC_SCALE=1;
   // Set by pass mode for the magnetometers
   I2CwriteByte(MPU9250_ADDRESS,0x37,0x02);
-  
   // Request continuous magnetometer measurements in 16 bits
   I2CwriteByte(MAG_ADDRESS,0x0A,0x16);
-  
-//  pinMode(13, OUTPUT);
-  Timer1.initialize(10000);         // initialize timer1, and set a 1/2 second period
-  Timer1.attachInterrupt(callback);  // attaches callback() as a timer overflow interrupt
-  
-  
-  // Store initial time
-  ti=millis();
 }
 
+vector<int16_t> *accel_flt = new vector<int16_t>();
+vector<int16_t> *gyro_flt  = new vector<int16_t>();
+vector<int16_t> *mag_flt   = new vector<int16_t>();
+vector<int16_t> *accel = new vector<int16_t>();
+vector<int16_t> *gyro  = new vector<int16_t>();
+vector<int16_t> *mag   = new vector<int16_t>();
 
-
-
-
-// Counter
-long int cpt=0;
-
-void callback()
-{ 
-  intFlag=true;
-  digitalWrite(13, digitalRead(13) ^ 1);
+/*
+ * arduino setup function
+ */
+void setup()
+{
+  // Arduino initializations
+  Wire.begin();
+  Serial.begin(38400);
+  MPU9250_init();
+  // index = 0;
+  ti = millis();
+  delay(1000);
 }
 
-// Main loop, read and display data
-void loop()
-{  
-  while (!intFlag);
-  intFlag=false;
-  
-  // Display time
-  Serial.print (millis()-ti,DEC);
-  Serial.print ("\t");
+/*
+ * arduino loop function
+ */
+void loop() {
+  // collect samples accel and gyro
+  getAccelGyro(accel, gyro);
+  filter(accel, accel_flt, 0.5);
+  filter(gyro, gyro_flt, 0.5);
+  // collect samples from mag
+  getMag(mag);
 
-  
-  // _______________
-  // ::: Counter :::
-  
-  // Display data counter
-//  Serial.print (cpt++,DEC);
-//  Serial.print ("\t");
-  
- 
- 
-  // ____________________________________
-  // :::  accelerometer and gyroscope ::: 
+  /*
+   * Display time & all values
+   */
+  // sprintf(printBuf, "[%8ld] accel: %6d/%6d/%6d gyro: %6d/%6d/%6d mag:%6d/%6d/%6d th: %3d, h: %3d",
+  //     millis() - ti, accel->x, accel->y, accel->z,
+  //     gyro->x, gyro->y, gyro->z, mag->x, mag->y, mag->z,
+  //     int(getTiltHeading(accel, mag)), getHeading(mag));
 
-  // Read accelerometer and gyroscope
-  uint8_t Buf[14];
-  I2Cread(MPU9250_ADDRESS,0x3B,14,Buf);
-  
-  // Create 16 bits values from 8 bits data
-  
-  // Accelerometer
-  int16_t ax=-(Buf[0]<<8 | Buf[1]);
-  int16_t ay=-(Buf[2]<<8 | Buf[3]);
-  int16_t az=Buf[4]<<8 | Buf[5];
+  /*
+   * Display time & accel, mag, tilt heading, heading
+   */
+  sprintf(printBuf, "[%8ld] [accel %6d/%6d/%6d] [mag%6d/%6d/%6d] [th %3d, h %3d]",
+      millis() - ti, accel->x, accel->y, accel->z, mag->x, mag->y, mag->z,
+      int(getTiltHeading(accel, mag)), getHeading(mag));
 
-  // Gyroscope
-  int16_t gx=-(Buf[8]<<8 | Buf[9]);
-  int16_t gy=-(Buf[10]<<8 | Buf[11]);
-  int16_t gz=Buf[12]<<8 | Buf[13];
-  
-    // Display values
-  
-  // Accelerometer
-  Serial.print (ax,DEC); 
-  Serial.print ("\t");
-  Serial.print (ay,DEC);
-  Serial.print ("\t");
-  Serial.print (az,DEC);  
-  Serial.print ("\t");
-  
-  // Gyroscope
-  Serial.print (gx,DEC); 
-  Serial.print ("\t");
-  Serial.print (gy,DEC);
-  Serial.print ("\t");
-  Serial.print (gz,DEC);  
-  Serial.print ("\t");
+  /*
+   * Display time & tilt heading & heading
+   */
+  // sprintf(printBuf, "[%8ld] th: %3d, h: %3d",
+  //     millis() - ti, int(getTiltHeading(accel, mag)), getHeading(mag));
 
-  
-  // _____________________
-  // :::  Magnetometer ::: 
+  /*
+   * Display time & heading
+   */
+  // sprintf(printBuf, "[%8ld] h: %3d", millis() - ti, getHeading(mag));
 
-  
-  // Read register Status 1 and wait for the DRDY: Data Ready
-  
-  uint8_t ST1;
-  do
-  {
-    I2Cread(MAG_ADDRESS,0x02,1,&ST1);
-  }
-  while (!(ST1&0x01));
+  /*
+   * Display time & tilt heading
+   */
+  // sprintf(printBuf, "[%8ld] h: %3d",
+  //     millis() - ti, int(getTiltHeading(accel, mag)));
 
-  // Read magnetometer data  
-  uint8_t Mag[7];  
-  I2Cread(MAG_ADDRESS,0x03,7,Mag);
-  
-
-  // Create 16 bits values from 8 bits data
-  
-  // Magnetometer
-  int16_t mx=-(Mag[3]<<8 | Mag[2]);
-  int16_t my=-(Mag[1]<<8 | Mag[0]);
-  int16_t mz=-(Mag[5]<<8 | Mag[4]);
-  
-  
-  // Magnetometer
-  Serial.print (mx+200,DEC); 
-  Serial.print ("\t");
-  Serial.print (my-70,DEC);
-  Serial.print ("\t");
-  Serial.print (mz-700,DEC);  
-  Serial.print ("\t");
-  
-  
-  
-  // End of line
-  Serial.println("");
-//  delay(100);    
+  // print
+  Serial.println(printBuf);
+  // wait a bit before restarting the loop
+  delay(100);
 }
-
-
-
-
-
-
-
-
-
